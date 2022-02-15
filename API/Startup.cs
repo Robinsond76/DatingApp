30fa9cec66899cf26be729f79fd1c03b99b262aa
Robinson Souza <Robinsond76@gmail.com>
2022-02-15 14:50:27 -0500

    Walking Skeleton - Client

--- API/Startup.cs
+++ API/Startup.cs
@@ -21,6 +21,9 @@ namespace API
                options.UseSqlite(_config.GetConnectionString("DefaultConnection")); 
             });
             services.AddControllers();
+            services.AddCors();
+
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -40,6 +43,11 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => 
+                policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
