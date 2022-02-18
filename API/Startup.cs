98484c9064ac310dc736ca27d63edea1a4c42776
Robinson Souza <Robinsond76@gmail.com>
2022-02-18 17:15:00 -0500

    used extensions to tidy startup.cs

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,11 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -18,15 +22,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ITokenService, TokenService>();
-            services.AddDbContext<DataContext>(options => 
-            {
-               options.UseSqlite(_config.GetConnectionString("DefaultConnection")); 
-            });
+            services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddCors();
-
-
+            services.AddIdentityServices(_config);
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -51,6 +51,8 @@ namespace API
                 policy.AllowAnyHeader()
                 .AllowAnyMethod()
                 .WithOrigins("https://localhost:4200"));
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
